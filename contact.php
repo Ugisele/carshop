5baf8c3e662c48f30ce4267f39c0c480798dd84f
Umutoni Gisele <umutonigisele12@gmail.com>
2022-02-11 11:23:18 +0200

    renting

---
+++ contact.php
@@ -0,0 +1,133 @@
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> car rent</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="contact.css">
+</head>
+<body>
+    <!-- starting menu -->
+    <div class="page">
+        <div class="navbar">
+            <div class="icon">
+                <h2 class="logo">
+                   CAR RENT
+                </h2>
+            </div>
+            <div class="menu">
+                <ul>
+                    <li><a href="index.html">HOME</a></li>
+                    <li><a href="about.html">ABOUTUS</a></li>
+                    <li><a href="products.html">PRODUCTS</a></li>
+                    <li><a href="contact.php">CONTACTUS</a></li>
+                </ul>
+            </div>
+            </div>
+            </div>
+            <br><br><br>
+
+                <div class="info">
+                    <div class="registor">
+                        <h3>Enter Client transaction Details</h3>
+                        <div id="error"></div>
+                        <form id="form" method="Post">
+                            <div>
+                                <div>
+                                    <label for="Name">Names</label>
+                                    <input id="Name" placeholder="Name" type="text" name="name" autocomplete="off">
+                                </div>
+                                <div>
+                                    <label for="Telephone">Telephone</label>
+                                    <input id="Telephone" placeholder="+250 " type="text" name="telephone" autocomplete="off">
+                                </div>
+                                <div>
+                                    <label for="Amount">Amount paid</label>
+                                    <input id="Amount" placeholder="from 250$" name="amount" autocomplete="off">
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <label for="Type">Cars available</label>
+                                            <select name='Cars for rent'>
+                                                <option value="Brabus S500 (5.0l V8)"> Brabus S500 (5.0l V8)</option>
+                                                <option value="Brabus T 13 Limousine (s 640">Brabus T 13 Limousine (s 640)</option>
+                                                <option value="Brabus T65S (S65 AMG)"> Brabus T65S (S65 AMG)</option>
+                                                <option value="Brabus Rocket 900 (900 hp/1500Nm)">Brabus Rocket 900 (900 hp/1500Nm)</option>
+                                                <option value="Brabus S500 (5.0l V8)"> Brabus S500 (8.0l V8)</option>
+                                                <option value="Brabus T 13 Limousine (s 640">Brabus T 20 Limousine (s 700)</option>
+                                                <option value="Brabus S500 (5.0l V8)"> Brabus S500 (5.0l V8)</option>
+                                                <option value="Brabus T 13 Limousine (s 640">Brabus T 13 Limousine (s 640)</option>
+                                                <option value="Brabus S500 (5.0l V8)"> Brabus S500 (5.0l V8)</option>
+                                                <option value="Brabus T 13 Limousine (s 640">Brabus T 13 Limousine (s 640)</option>
+                                              
+                                            </select>
+                                        
+                                </div>
+                                <div>
+                                    <label for="Date">Date</label>
+                                    <input type="date" id="Date" required name="date">
+                                </div>
+                                <button type="submit" name="submit" id="btn">submit</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="print">
+                        <h3>Client Transaction List</h3>
+                        <table>
+                            <tr>
+                                <th>Num</th>
+                                <th>Name</th>
+                                <th>Telephone</th>
+                                <th>Amount</th>
+                                <th>Haircut</th>
+                                <th>Date</th>
+                                <th>Operations</th>
+                            </tr>
+                            
+                            <?php
+                                $sql= "select * from `clients`";
+                                $result=mysqli_query($conn,$sql);
+
+                                if ($result) {
+                                while($row=mysqli_fetch_assoc($result)){
+                                    $id=$row['id'];
+                                    $name=$row['names'];
+                                    $telephone=$row['telephone'];
+                                    $amount=$row['amount'];
+                                    $haircut=$row['haircut'];
+                                    $date=$row['dateop'];
+
+                                    echo '
+                                    <tr>
+                                        <td>'.$id.'</td>
+                                        <td>'.$name.'</td>
+                                        <td>'.$telephone.'</td>
+                                        <td>'.$amount.'</td>
+                                        <td>'.$haircut.'</td>
+                                        <td>'.$date.'</td>
+                                        <td>
+                                            <div>
+                                                <button class="green"><a href="Update.php?updateid='.$id.'">Update</a></button>
+                                                <button class="red"><a href="delete.php?deleteid='.$id.'">Delete</a></button>
+                                            </div>
+                                        </td>
+                                    </tr>';
+                                }
+                                
+                                } 
+	                        ?>
+
+				  </table>
+			</div>
+        </div>
+                        </table>
+                    </div>
+                </div>
+                        
+        
+    
+</body>
+</html>
---
+++ create_table.php
@@ -0,0 +1,32 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+
+// connection
+
+$conne=new mysqli($servername,$username,$password);
+// testing if connection is working
+
+if($conne->connect_error){
+    die("connection failed" .$conne->connect_error);
+    
+}
+// Create database
+$sql = "CREATE DATABASE Customers (ID INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+names varchar(30),
+e_mail varchar(30),
+phone int(10),
+messages varchar(1000),
+reg_date timestamp default current_timestamp on update current_timestamp )";
+
+if ($conn->query($sql) === TRUE) {
+
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+
+?>
